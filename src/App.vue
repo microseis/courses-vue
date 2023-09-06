2078bf0355d2b9ea3666611cbd62db5d43144d2a
Aleksei Stepanov <stepanov.geo@ya.ru>
2023-09-06 23:19:30 +0300

    dev: initial setup

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,51 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
-</template>
+  <div id="wrapper" >
+    <nav class="navbar is-dark">
+      <div class="navbar-brand">
+        <router-link to="/" class="navbar-item"><strong><tag class="has-text-white">My App</tag></strong></router-link>
+        <a class="navbar-burger" aria-label="false" data-target="navbar-menu">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+      <div class="navbar-menu" id="navbar-menu">
+        <div class="navbar-end">
+          <router-link to="/some-another-link" class="navbar-item">Some Another Link</router-link>
+          <router-link to="/some-link" class="navbar-item">Some Link</router-link>
+          <div class="navbar-item">
+            <div class="buttons">
+              <router-link to="/log-in" class="button is-light">Log In</router-link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+    <section class="section">
+      <router-view/>
+    </section>
+    
+    <footer class="footer">
+      <p class="has-text-centered">Copyright 2023 (c)</p>
+    </footer>  
+  </div> 
+  
+  </template>
+  
+  <script>
+  
+  export default {
+    name: 'App',
+    data() {
+      return {name: "Alexey",
+              greet: "Hello,",
+             isDisabled: false};
     }
   }
-}
-</style>
+  </script>
+  
+  <style lang="scss">
+  @import '../node_modules/bulma';
+  </style>
+  
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div> {{ greet }} {{ name }}</div>
+    <h2 class="underline">Happy Coding</h2>
+    <button v-bind:disabled="isDisabled" class="button is-success">Some Button</button>
   </div>
 </template>
+
+<script>
+  export default {
+    name: 'App',
+    data() {
+      return {name: "Alexey",
+              greet: "Hello,",
+             isDisabled: false};
+    }
+  }
+</script>
